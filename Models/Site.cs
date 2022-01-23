980e2b6051196fb618e0538b15d7d77b9699735c
Andrea <andrenaje94@hotmail.com>
2022-01-23 19:13:38 +0100

    Models sites

---
+++ Models/Site.cs
@@ -0,0 +1,12 @@
+
+namespace AACLIENTE.AAAPI.Sites;
+
+
+public class Site {
+    public int id {get; set;}
+    public string name {get; set;}
+    public string userName {get; set;}
+    public string password {get; set;}
+    public int IdCategory {get; set;}
+    
+    }
