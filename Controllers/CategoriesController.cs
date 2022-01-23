62f58eeb7a357b89b61f64cf80a855d879b1be8b
Andrea <andrenaje94@hotmail.com>
2022-01-23 19:08:05 +0100

    controladores categories

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,77 @@
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AACLIENTE.AAAPI.Categories.Controllers;
+
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private static List<Category> Categories = new List<Category>{
+            new Category { id=1 ,name="categoria1",description="uno"},
+            new Category { id=2 ,name="categoria2",description="dos"}
+
+        };
+ 
+
+[HttpGet]
+    public ActionResult<List<Category>> Get(){
+        return Ok(Categories);
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public ActionResult<Category> Get (int id){
+        var category = Categories.Find (x =>x.id == id);
+        return category == null ? NotFound() : Ok(category);
+    }
+    
+
+
+
+    [HttpPost]
+     public ActionResult<Category> Post([FromBody] Category category){
+        var existingCategory = Categories.Find(x=>x.id== category.id);
+        if (existingCategory != null){
+          
+          String url = Request.Path.ToString() + "/" + category.id;
+            return Conflict("ya existe esa categoria");
+        } else {
+            Categories.Add(category);
+            var resourceUrl = Request.Path.ToString()+ "/" + category.id;
+            return Created(resourceUrl, category);
+        }
+        }
+ 
+     [HttpPut]
+    public ActionResult Put (Category category){
+        var existingCategory = Categories.Find(x=>x.id== category.id);
+        if (existingCategory == null){
+            return Conflict("no existe esa categoria");
+        } else {
+            existingCategory.name = category.name;
+            return Ok();
+        }
+        }
+     [HttpDelete]
+    [Route("{id}")]
+    public ActionResult<Category> Delete (int id){
+        var category = Categories.Find (x =>x.id == id);
+        if (category == null){
+            return NotFound();
+        } else{
+            Categories.Remove(category);
+            return NoContent();
+        }
+    }
+    
+    }
+
+
+    
+   
+
+
